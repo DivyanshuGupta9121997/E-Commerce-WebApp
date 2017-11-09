8f23eb9202de22e68d0a96f6d8c9d55111c7f129
Krishnkant-Swarnkar <krishnkant.swarnkar.cse15@itbhu.ac.in>
2017-11-09 06:31:55 +0000

    changes in WSGI file

--- DhakadGarments/urls.py
+++ DhakadGarments/urls.py
@@ -21,3 +21,6 @@ urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^', include('garments.urls')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
